e220947046dd708610fb902c5ed408aab120350e
MariPinas <marianapls2003@gmail.com>
2024-03-01 20:41:24 -0300

    Solved 4,5,6,7,8 and 9

--- Aula_4_Lista_Exercicios_funcoes/dist/js/index.js
+++ Aula_4_Lista_Exercicios_funcoes/dist/js/index.js
@@ -25,4 +25,39 @@ function media(...valores) {
     }
     return media / i;
 }
-console.log("Exercicio 3: ", media(1,2,3,4,5,6));
+//console.log("Exercicio 3: ", media(1,2,3,4,5,6));
+function upper(palavra) {
+    return palavra.toUpperCase();
+}
+//console.log("Exercicio 4", upper("Bezinha muito gorda"));
+function primo(var1) {
+    let i;
+    for (i = 2; i < var1; i++) {
+        if (var1 % i == 0)
+            return false;
+    }
+    return true;
+}
+//console.log("Exercicio 5", primo(2), primo(3), primo(11), primo(14), primo(17));
+function inverter(...num) {
+    return num.reverse();
+}
+//console.log("Exercicio 6", inverter(1,2,3,4,5,6,7,8,9));
+function porcentagem(num, p) {
+    return num * ((p / 100) + 1);
+}
+//console.log("Exercicio 7", porcentagem(10, 75));
+function invertePalavra(texto) {
+    return texto.split('').reverse().toString();
+}
+console.log("Exercicio 8", invertePalavra("mariana"));
+function somaPar(...num) {
+    let soma = 0;
+    for (let i = 0; i < 4; i++) {
+        if (num[i] % 2 == 0) {
+            soma = soma + num[i];
+        }
+    }
+    return soma;
+}
+//console.log("Exercicio 9", somaPar(1,2,3,4,5));
--- Aula_4_Lista_Exercicios_funcoes/src/index.ts
+++ Aula_4_Lista_Exercicios_funcoes/src/index.ts
@@ -33,3 +33,50 @@ function media(...valores:number[]) : number{
 }
 
 //console.log("Exercicio 3: ", media(1,2,3,4,5,6));
+
+function upper(palavra:string):string{
+    return palavra.toUpperCase();
+}
+//console.log("Exercicio 4", upper("Bezinha muito gorda"));
+
+function primo(var1:number):boolean{
+    let i:number;
+    for(i = 2; i < var1; i++){
+        if (var1 % i == 0)
+            return false;
+    }
+    return true;
+}
+
+//console.log("Exercicio 5", primo(2), primo(3), primo(11), primo(14), primo(17));
+
+function inverter(...num:number[]): number[]{
+    return num.reverse();
+}
+
+//console.log("Exercicio 6", inverter(1,2,3,4,5,6,7,8,9));
+
+function porcentagem(num:number, p:number):number{
+    return num * ((p/100)+1);
+}
+
+//console.log("Exercicio 7", porcentagem(10, 75));
+
+function invertePalavra(texto:string):string{
+    return texto.split('') .reverse() .toString();
+}
+
+//console.log("Exercicio 8", invertePalavra("mariana"));
+
+function somaPar(...num:number[]):number{
+    let soma:number = 0;
+
+    for(let i=0; i < 4; i++){
+        if(num[i] % 2 == 0){
+            soma = soma + num[i];
+        }
+    }
+    return soma;
+}
+
+//console.log("Exercicio 9", somaPar(1,2,3,4,5));
