ad684b751dc4a4448e2c125d21f992f4cab96bfc
Mariana <marianapls2003@gmail.com>
2024-03-03 19:04:26 -0300

    finished exrc 10

--- Aula_4_Lista_Exercicios_funcoes/dist/js/index.js
+++ Aula_4_Lista_Exercicios_funcoes/dist/js/index.js
@@ -9,14 +9,14 @@ function maior(...valores) {
     }
     return maior;
 }
-//console.log ("Exercicio 1 : " , maior(1,2,3,4,5,6));
+console.log("Exercicio 1 : ", maior(1, 2, 3, 4, 5, 6));
 function par(val) {
     if (val % 2 == 0)
         return true;
     else
         return false;
 }
-//console.log ("Exercicio 2: ", par(2));
+console.log("Exercicio 2: ", par(2), par(7), par(8));
 function media(...valores) {
     let i;
     let media = 0;
@@ -25,11 +25,11 @@ function media(...valores) {
     }
     return media / i;
 }
-//console.log("Exercicio 3: ", media(1,2,3,4,5,6));
+console.log("Exercicio 3: ", media(1, 2, 3, 4, 5, 6), media(2, 6, 9, 10, 60, 1), media(0, 1, 5, 80, 34, 2));
 function upper(palavra) {
     return palavra.toUpperCase();
 }
-//console.log("Exercicio 4", upper("Bezinha muito gorda"));
+console.log("Exercicio 4", upper("hello world"), upper("teste"), upper("vai corinthians"));
 function primo(var1) {
     let i;
     for (i = 2; i < var1; i++) {
@@ -38,15 +38,15 @@ function primo(var1) {
     }
     return true;
 }
-//console.log("Exercicio 5", primo(2), primo(3), primo(11), primo(14), primo(17));
+console.log("Exercicio 5", primo(2), primo(3), primo(11), primo(14), primo(17));
 function inverter(...num) {
     return num.reverse();
 }
-//console.log("Exercicio 6", inverter(1,2,3,4,5,6,7,8,9));
+console.log("Exercicio 6", inverter(1, 2, 3, 4, 5, 6, 7, 8, 9));
 function porcentagem(num, p) {
     return num * ((p / 100) + 1);
 }
-//console.log("Exercicio 7", porcentagem(10, 75));
+console.log("Exercicio 7", porcentagem(10, 75), porcentagem(20, 55), porcentagem(45, 80));
 function invertePalavra(texto) {
     return texto.split('').reverse().toString();
 }
@@ -60,4 +60,10 @@ function somaPar(...num) {
     }
     return soma;
 }
-//console.log("Exercicio 9", somaPar(1,2,3,4,5));
+console.log("Exercicio 9", somaPar(1, 2, 3, 4, 5));
+function fatorial(num) {
+    if (num == 0)
+        return 1;
+    return num * fatorial(num - 1);
+}
+console.log("Exercicio 10", fatorial(2), fatorial(4), fatorial(6));
--- Aula_4_Lista_Exercicios_funcoes/src/index.ts
+++ Aula_4_Lista_Exercicios_funcoes/src/index.ts
@@ -11,7 +11,7 @@ let i:number
     
 }
 
-//console.log ("Exercicio 1 : " , maior(1,2,3,4,5,6));
+console.log ("Exercicio 1 : " , maior(1,2,3,4,5,6));
 
 function par(val:number): boolean{
     if (val%2==0)
@@ -21,7 +21,7 @@ function par(val:number): boolean{
 
 }
 
-//console.log ("Exercicio 2: ", par(2));
+console.log ("Exercicio 2: ", par(2), par(7), par(8));
 
 function media(...valores:number[]) : number{
     let i:number;
@@ -32,12 +32,13 @@ function media(...valores:number[]) : number{
     return media/i;
 }
 
-//console.log("Exercicio 3: ", media(1,2,3,4,5,6));
+console.log("Exercicio 3: ", media(1,2,3,4,5,6), media(2,6,9,10,60,1), media(0,1,5,80,34,2));
 
 function upper(palavra:string):string{
     return palavra.toUpperCase();
 }
-//console.log("Exercicio 4", upper("Bezinha muito gorda"));
+console.log("Exercicio 4", upper("hello world"), upper("teste"), upper("vai corinthians"));
+
 
 function primo(var1:number):boolean{
     let i:number;
@@ -48,25 +49,26 @@ function primo(var1:number):boolean{
     return true;
 }
 
-//console.log("Exercicio 5", primo(2), primo(3), primo(11), primo(14), primo(17));
+console.log("Exercicio 5", primo(2), primo(3), primo(11), primo(14), primo(17));
 
 function inverter(...num:number[]): number[]{
     return num.reverse();
 }
 
-//console.log("Exercicio 6", inverter(1,2,3,4,5,6,7,8,9));
+console.log("Exercicio 6", inverter(1,2,3,4,5,6,7,8,9));
 
 function porcentagem(num:number, p:number):number{
     return num * ((p/100)+1);
 }
 
-//console.log("Exercicio 7", porcentagem(10, 75));
+console.log("Exercicio 7", porcentagem(10, 75), porcentagem(20,55), porcentagem(45,80));
+
 
 function invertePalavra(texto:string):string{
     return texto.split('') .reverse() .toString();
 }
 
-//console.log("Exercicio 8", invertePalavra("mariana"));
+console.log("Exercicio 8", invertePalavra("mariana"));
 
 function somaPar(...num:number[]):number{
     let soma:number = 0;
@@ -79,4 +81,12 @@ function somaPar(...num:number[]):number{
     return soma;
 }
 
-//console.log("Exercicio 9", somaPar(1,2,3,4,5));
+console.log("Exercicio 9", somaPar(1,2,3,4,5));
+
+function fatorial(num:number):number{
+    if(num==0)
+        return 1;
+    return num * fatorial(num -1);
+}
+
+console.log("Exercicio 10", fatorial(2), fatorial(4), fatorial(6))
